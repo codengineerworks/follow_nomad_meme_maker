ff4baab43688dd8a1f3e3769caa65d57f09188c3
finedevworks <finedevworks@gmail.com>
2023-10-09 18:30:28 +0900

    Feat: canvas api path 1 - 함수 적용 범위
    첫 번째 fill() 함수
    - 설정된 사각형 도형들 검은색 채우기
    
    두 번째 fill() 함수
    - setTimeout 함수로 동작 관찰
    - 관찰 결과 : 기존 검은색 사각형들도 빨강색으로 변경
    - 관찰 결론 : 기존 설정된 사각형들도 함수 적용 범위에 존재

--- app.js
+++ app.js
@@ -5,4 +5,15 @@ canvas.width = 800; // 캔버스 가로 설정
 canvas.height = 800; // 캔버스 세로 설정
 
 ctx.rect(50, 50, 100, 100);
-ctx.fill();
+ctx.rect(150, 150, 100, 100);
+ctx.rect(250, 250, 100, 100);
+ctx.fill(); // 첫 번째 fill 동작 - 검은색 채우기
+
+ctx.rect(350, 350, 100, 100);
+ctx.fillStyle = "red";
+
+setTimeout(() => {
+	// 두 번째 fill 동작 - 빨강색 채우기
+	// 기존 검은색 사각형들도 빨강색으로 변경
+	ctx.fill();
+}, 5000);
