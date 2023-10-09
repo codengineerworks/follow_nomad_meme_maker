c91011455100351a23a8a065d7a203efead04733
finedevworks <finedevworks@gmail.com>
2023-10-09 17:36:10 +0900

    Build: canvas 실습 환경 설정
    css 설정
    - canvas: 가로 세로 테두리 설정
    - body: Flex 컨테이너 설정
    
    js 설정
    - canvas 요소 호출
    - canvas 요소에 그리는 도구 호출
    - canvas 가로 세로 설정
    
    canvas api 기초
    - 좌표 기준 : 좌상단 테두리 바깥 꼭지점
    - 우측으로 가로 값 증가
    - 하단으로 세로 값 증가

---
+++ app.js
@@ -0,0 +1,5 @@
+const canvas = document.querySelector("canvas"); // canvas 요소 호출
+const ctx = canvas.getContext("2d"); // canvas 요소에 그리는 도구 호출
+
+canvas.width = 800; // 캔버스 가로 설정
+canvas.height = 800; // 캔버스 세로 설정
