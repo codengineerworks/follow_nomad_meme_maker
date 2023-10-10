89e70ac8bb2bccb91c24c0445c15826d31bed60b
finedevworks <finedevworks@gmail.com>
2023-10-10 13:42:43 +0900

    Feat: 실습 - 집 그리기
    집 그리기 : 학습된 함수와 속성 이용
    - fillRect(x, y, w, h)
    - moveTo(x, y)
    - lineTo(x, y)
    - fill()
    - lineWidth 설정

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const ctx = canvas.getContext("2d"); // canvas 요소에 그리는 도구 호출
 canvas.width = 800; // 캔버스 가로 설정
 canvas.height = 800; // 캔버스 세로 설정
 
-ctx.moveTo(50, 50); // 입력 위치로 랜더링 도구 이동
-ctx.lineTo(150, 50); // 기존 위치부터 입력 위치까지 직선 설정
-ctx.lineTo(150, 150);
-ctx.lineTo(50, 150);
-ctx.lineTo(50, 50);
-ctx.fill(); // 채우기
+ctx.fillRect(200, 200, 50, 200); // 첫 번째 벽
+ctx.fillRect(400, 200, 50, 200); // 두 번째 벽
+ctx.lineWidth = 2;
+ctx.fillRect(300, 300, 50, 100); // 문
+ctx.fillRect(200, 200, 200, 20); // 천장
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100); // 좌측 지붕
+ctx.lineTo(450, 200); // 우측 지붕
+ctx.fill();
