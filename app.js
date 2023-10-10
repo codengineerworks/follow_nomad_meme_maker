6c8ff28c4ab0f2948d21ce4eef3801685efe89cf
finedevworks <finedevworks@gmail.com>
2023-10-10 13:37:51 +0900

    Feat: fillRect(x, y, w, h) = 4 lineTo(x, y) + fill()
    fillRect(x, y, w, h)
    = rect(x, y, w, h) + fill()
    = moveTo(x, y) + 4 * lineTo(x, y) + fill()
    
    rect(x, y, w, h)
    - moveTo 와 lineTo 로 만든 shortcut 함수
    
    moveTo(x, y) : 입력 위치로 랜더링 도구 이동
    
    lineTo(x, y) : 기존 위치부터 입력 위치까지 직선 설정

--- app.js
+++ app.js
@@ -4,16 +4,9 @@ const ctx = canvas.getContext("2d"); // canvas 요소에 그리는 도구 호출
 canvas.width = 800; // 캔버스 가로 설정
 canvas.height = 800; // 캔버스 세로 설정
 
-ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
-ctx.rect(250, 250, 100, 100);
-ctx.fill(); // 첫 번째 fill 동작 - 검은색 채우기
-
-ctx.beginPath(); // 새로운 path 설정
-ctx.rect(350, 350, 100, 100);
-ctx.rect(450, 450, 100, 100);
-ctx.fillStyle = "red";
-
-// 두 번째 fill 동작 - 빨강색 채우기
-// 새로운 path 에 동작 적용
-ctx.fill();
+ctx.moveTo(50, 50); // 입력 위치로 랜더링 도구 이동
+ctx.lineTo(150, 50); // 기존 위치부터 입력 위치까지 직선 설정
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
+ctx.fill(); // 채우기
