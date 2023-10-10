b6823721dc3b20d1f3e5f0f014dde8ba4f1adf7e
finedevworks <finedevworks@gmail.com>
2023-10-10 13:54:53 +0900

    Feat: 실습 - 사람 그리기
    arc(x, y, r, s, e)
    - 기능 : 호 그리기
    - 주석 1 : 기준 위치는 가상의 원의 가장 오른쪽 점
    - 주석 2 : 각도는 기준 위치부터 아래로 그리며 증가
    - (args) x : 기준 위치 가로 좌표
    - (args) y : 기준 위치 세로 좌표
    - (args) r : 호의 반지름
    - (args) s : 호의 시작 각도(rad)
    - (args) e : 호의 종료 각도(rad)
    
    사람 그리기 : 학습된 함수와 속성 이용
    - fillRect(x, y, w, h)
    - arc(x, y, r, s, e)
    - beginPath()
    - fill()
    - fillStyle 설정

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const ctx = canvas.getContext("2d"); // canvas 요소에 그리는 도구 호출
 canvas.width = 800; // 캔버스 가로 설정
 canvas.height = 800; // 캔버스 세로 설정
 
-ctx.fillRect(200, 200, 50, 200); // 첫 번째 벽
-ctx.fillRect(400, 200, 50, 200); // 두 번째 벽
-ctx.lineWidth = 2;
-ctx.fillRect(300, 300, 50, 100); // 문
-ctx.fillRect(200, 200, 200, 20); // 천장
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100); // 좌측 지붕
-ctx.lineTo(450, 200); // 우측 지붕
+ctx.fillRect(200, 200, 15, 100);
+ctx.fillRect(365, 200, 15, 100);
+ctx.fillRect(260, 200, 60, 200);
+
+ctx.arc(290, 135, 50, 0, 2 * Math.PI); // 호 그리기 > 원
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "white"; // 채우기 하얀색
+ctx.arc(275, 130, 8, 1 * Math.PI, 2 * Math.PI); // 호 그리기 > 반원
+ctx.arc(305, 130, 8, 1 * Math.PI, 2 * Math.PI);
 ctx.fill();
